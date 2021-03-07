a4c102fd793c1d869399b12dcd6b1a2cc7ac8aa1
Kevin Beaulieu <kevinb@thumbtack.com>
2021-03-06 20:13:11 -0800

    Create blank Xcode project

---
+++ Thumbprint/Classes/Thumbprint.h
@@ -0,0 +1,18 @@
+//
+//  Thumbprint.h
+//  Thumbprint
+//
+//  Created by Kevin Beaulieu on 3/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Thumbprint.
+FOUNDATION_EXPORT double ThumbprintVersionNumber;
+
+//! Project version string for Thumbprint.
+FOUNDATION_EXPORT const unsigned char ThumbprintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thumbprint/PublicHeader.h>
+
+
