80257715c8d6350e6beb93f470c0e65a56e9ff5d
Kevin Beaulieu <kevinb@thumbtack.com>
2021-05-14 22:35:06 -0700

    Create PR instead of merging directly

--- .github/scripts/commit_release.sh
+++ .github/scripts/commit_release.sh
@@ -2,11 +2,6 @@
 
 set -e -o pipefail
 
-# Checkout main branch
-git -c core.sshCommand="ssh -i $2" remote update
-git -c core.sshCommand="ssh -i $2" fetch
-git -c core.sshCommand="ssh -i $2" checkout --track origin/main
-
 # Update version in podspec.
 # (Search podspec for `.version = '1.2.3` and update with new version
 # number passed in as script argument).
@@ -16,4 +11,4 @@ rm Thumbprint.podspec.bak
 # Commit changes and push.
 git add --all
 git commit -m "Release $1"
-git -c core.sshCommand="ssh -i $2" push origin main
+gh pr create --title "Release $1" --body ""
