81bca4bee76016594fb06825c4344c722912d822
Kevin Beaulieu <kevinb@thumbtack.com>
2021-05-15 19:10:33 -0700

    Remove unnecessary gh auth command

--- .github/scripts/commit_release.sh
+++ .github/scripts/commit_release.sh
@@ -12,5 +12,4 @@ rm Thumbprint.podspec.bak
 git add --all
 git commit -m "Release $1"
 git -c core.sshCommand="ssh -i $2" push origin $(git branch --show-current)
-gh auth login
 gh pr create --title "Release $1" --body ""
