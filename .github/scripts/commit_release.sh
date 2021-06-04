7a926f9e8ef206e0f5af8a3a0d2db7ffffd925b6
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2021-06-04 09:46:33 -0700

    Fix automated release version bump PRs (#34)
    When releasing new versions, the release action automatically creates
    a PR bumping the pod version. However by default, push actions
    triggered by another action do not trigger the usual push event.
    If I understand the documentation correctly, setting
    `persist-credentials: false` should disable this behavior so that
    the version bump PR still triggers the CI action.

--- .github/scripts/commit_release.sh
+++ .github/scripts/commit_release.sh
@@ -7,11 +7,11 @@ git checkout -b "release/$1"
 # Update version in podspec.
 # (Search podspec for `.version = '1.2.3` and update with new version
 # number passed in as script argument).
-sed -i.bak -E "s/\.version *= *(["'"'"'])[0-9]\.[0-9]\.[0-9]["'"'"']/.version = \1$1\1/g" Thumbprint.podspec
+sed -i.bak -E "s/\.version *= *(["'"'"'])[0-9](\.[0-9])?(\.[0-9])?["'"'"']/.version = \1$1\1/g" Thumbprint.podspec
 rm Thumbprint.podspec.bak
 
 # Commit changes and push.
 git add --all
 git commit -m "Release $1"
-git push origin $(git branch --show-current)
-gh pr create --title "Release $1" --body ""
+git push https://$GITHUB_TOKEN@github.com/thumbtack/thumbprint-ios.git head:$(git branch --show-current)
+gh pr create --title "Release $1" --body "" --head $(git branch --show-current)
