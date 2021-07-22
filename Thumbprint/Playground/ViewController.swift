5e141f10ca4161f6ce31cc2e01555bab8d547517
Prudhvi Gadiraju <prudhvi@thumbtack.com>
2021-07-22 14:31:40 -0700

    Add Playground Target

---
+++ Thumbprint/Playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Playground
+//
+//  Created by Prudhvi Gadiraju on 7/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
