ea8edac29873513473d788cd8fb70d62cc047740
Scott Southerland <scottasoutherland@thumbtack.com>
2022-05-25 16:12:15 -0700

    Fix bundle load

--- Thumbprint/Targets/Thumbprint/Common/Bundle+Extensions.swift
+++ Thumbprint/Targets/Thumbprint/Common/Bundle+Extensions.swift
@@ -1,8 +1,10 @@
 import Foundation
 
+private class ThumbprintResourcesClass {}
+
 public extension Bundle {
     static let thumbprint: Bundle = {
-        let resourcesPath = Bundle.main.path(forResource: "ThumbprintResources", ofType: "bundle")! // swiftlint:disable:this force_unwrapping
+        let resourcesPath = Bundle(for: ThumbprintResourcesClass.self).path(forResource: "ThumbprintResources", ofType: "bundle")! // swiftlint:disable:this force_unwrapping
         return Bundle(path: resourcesPath)! // swiftlint:disable:this force_unwrapping
     }()
 }
