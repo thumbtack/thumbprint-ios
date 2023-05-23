8ee7848871bb9c429cad7c7e0e3101b37d5c5ff9
Muhieddine EL Kaissi <mkaissi@thumbtack.com>
2023-05-23 09:57:22 -0700

    caching build

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -4,4 +4,4 @@ set -e -u -o pipefail
 
 # swiflint is executed during the build phase via plugin
 # It's not the case with swiftformat which needs to be triggered manually
-swift package --allow-writing-to-package-directory swiftformat --lint
+swift package -c release --allow-writing-to-package-directory swiftformat --lint
